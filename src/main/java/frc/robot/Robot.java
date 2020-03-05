d04ef078e6054125764d41932edf9172bfe1e381
SIX10 <six10@protonmail.com>
2020-03-05 15:53:20 -0600

    I cured tourettes
    Yuh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.cscore.UsbCamera;
@@ -42,13 +38,7 @@ public class Robot extends IterativeRobot {
 	SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
 	SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
 	DifferentialDrive drive = new DifferentialDrive(left, right);
-
-	Compressor mainCompressor = new Compressor(0);
-	DoubleSolenoid Piston = new DoubleSolenoid(0, 1);
-
-	
-	RobotDrive arcadeDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
-	
+		
 	Joystick xbox = new Joystick(0);
 	
 	double x;
@@ -64,26 +54,6 @@ public class Robot extends IterativeRobot {
 	public void robotInit() {
 		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 		camera.setResolution(320, 240);
-
-		//chooser.addDefault("Default Auto", defaultAuto);
-		//chooser.addObject("My Auto", customAuto);
-		//SmartDashboard.putData("Auto choices", chooser);
-		//new Thread(() -> {
-            //UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-            //camera.setResolution(640, 480);
-            
-            //CvSink cvSink = CameraServer.getInstance().getVideo();
-            //CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-            
-            //Mat source = new Mat();
-            //Mat output = new Mat();
-            
-            //while(!Thread.interrupted()) {
-                //cvSink.grabFrame(source);
-                //Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-                //outputStream.putFrame(output);
-            //}
-        //}).start();
 	}
 
 	/**
@@ -99,10 +69,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		arcadeDrive.setInvertedMotor(MotorType.kFrontRight, true);
-		arcadeDrive.setInvertedMotor(MotorType.kFrontLeft, false);
-		arcadeDrive.setInvertedMotor(MotorType.kRearRight, true);
-		arcadeDrive.setInvertedMotor(MotorType.kRearLeft, false);
 	}
 
 	/**
@@ -112,89 +78,16 @@ public class Robot extends IterativeRobot {
 	public void autonomousPeriodic() {
 
 	}
- public void teleopInit(){
-	 arcadeDrive.setInvertedMotor(MotorType.kFrontRight, true);
-	 arcadeDrive.setInvertedMotor(MotorType.kFrontLeft, false);
-	 arcadeDrive.setInvertedMotor(MotorType.kRearRight, true);
-	 arcadeDrive.setInvertedMotor(MotorType.kRearLeft, false);
- }
+	public void teleopInit(){
+
+ 	}
 	/**
 	 * This function is called periodically during operator control
 	 */
 	@Override
 	public void teleopPeriodic() {
-		/*
-		x = JS1.getX();
-		y = JS1.getY();
-		arcadeDrive.arcadeDrive(x, y);
 		
-		if(JS1.getRawButton(liftButton)) {
-			leftLiftMotor.set(-0.70); 
-			rightLiftMotor.set(0.30);
-		} else if(JS1.getRawButton(lowerButton)) {
-			leftLiftMotor.set(0.15);
-			rightLiftMotor.set(-0.15);
-		} else {
-			leftLiftMotor.set(-0.05);
-			rightLiftMotor.set(0.025);
-			}
-
-		
-		//mainCompressor.setClosedLoopControl(false);
-
-		if(JS1.getRawButton(pistonTrigger)) {
-			Piston.set(Value.kForward);
-			Timer.delay(0.5);
-			Piston.set(Value.kReverse);
-		} else {
-			Piston.set(DoubleSolenoid.Value.kOff);
-		}
-		
-		if(JS1.getRawButton(compressorOn)) {
-			mainCompressor.setClosedLoopControl(true);
-		} else if (JS1.getRawButton(compressorOff)){
-			mainCompressor.setClosedLoopControl(false);
-		}	
-
-		if(JS1.getRawButton(rollerOut)){
-			leftRollerMotor.set(-0.5);
-			rightRollerMotor.set(0.5);
-		} else if(JS1.getRawButton(rollerIn)){
-			leftRollerMotor.set(0.5);
-			rightRollerMotor.set(-0.5);
-		} else {
-			leftRollerMotor.set(0);
-			rightRollerMotor.set(0.05);
-		}
-		
-		if(JS1.getRawButton(strafeLeft)){
-			frontStrafeMotor.set(-0.25);
-			rearStrafeMotor.set(0.25);
-		} else if(JS1.getRawButton(strafeRight)){
-			frontStrafeMotor.set(0.25);
-			rearStrafeMotor.set(-0.25);
-		} else {
-			frontStrafeMotor.set(0);
-			rearStrafeMotor.set(0);
-		}
-
-		if(JS1.getRawButton(switchForward)) {
-			arcadeDrive.setInvertedMotor(MotorType.kFrontRight, true);
-			arcadeDrive.setInvertedMotor(MotorType.kFrontLeft, false);
-			arcadeDrive.setInvertedMotor(MotorType.kRearRight, true);
-			arcadeDrive.setInvertedMotor(MotorType.kRearLeft, false);
-		} else if(JS1.getRawButton(switchBackward)){
-			arcadeDrive.setInvertedMotor(MotorType.kFrontRight, false);
-			arcadeDrive.setInvertedMotor(MotorType.kFrontLeft, true);
-			arcadeDrive.setInvertedMotor(MotorType.kRearRight, false);
-			arcadeDrive.setInvertedMotor(MotorType.kRearLeft, true);
-		}
-		*/
-
-
-
-
-		drive.tankDrive(fixController(-xbox.getRawAxis(1))*0.8, fixController(xbox.getRawAxis(5))*0.8);
+		drive.tankDrive(-(xbox.getRawAxis(1)), -(xbox.getRawAxis(5)));
 
 		if (xbox.getRawButton(1)) {     //A  turn on flywheel
 			flyWHeel.set(0.65);
@@ -233,11 +126,13 @@ public class Robot extends IterativeRobot {
 
 	public double fixController(double val) {
 
-		System.out.println(val);
+		
 
-		if (val >= 0.5 || val <= 0.5) {
+		if (val >= 0.5 || val <= -0.5) {
+			System.out.println(val);
 			return val;
 		} else {
+			System.out.println("0.0");
 			return 0.0;
 		}
 		
